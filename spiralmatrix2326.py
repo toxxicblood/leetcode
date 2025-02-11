f36537e5537e143758aed6062300e62da4c804ed
toxxinated <mungairamsey01@gmail.com>
2025-02-11 21:26:53 +0300

    full working code

--- spiralmatrix2326.py
+++ spiralmatrix2326.py
@@ -4,56 +4,59 @@ class ListNode(object):
         self.val = val
         self.next = next
 
+
 class Solution(object):
-    def spiralMatrix(self,m,n, head):
+    def spiralMatrix(self, m, n, head):
         """
         :type m: int
         :type n: int
         :type head: Optional[ListNode]
         :rtype: List[List[int]]
         """
-        matrix = self.generateMatrix(m,n)
-        #i need to know how to index into a sublist
-        #matrix [0][0] = 1
-        #return matrix
+        matrix = self.generateMatrix(m, n)
+        # i need to know how to index into a sublist
+        # matrix [0][0] = 1
+        # return matrix
         current = head
         nodes = []
         while current is not None:
             nodes.append(current.val)
             current = current.next
-            #print(current)
-        print (nodes)
+            # print(current)
+        print(nodes)
         h = len(nodes)
-        #return h
-        #return len(matrix)
+        # return h
+        # return len(matrix)
         i = 0
         j = 0
         directions = ["right", "down", "left", "up"]
         d = 0
         count = 0
 
-        while count <= h-1:
-            print (directions[d])
-            
-            if matrix [i][j] == -1 :
+        while count <= h - 1:
+            print(directions[d])
+
+            if matrix[i][j] == -1:
+                # match directions[d]:
+                # case "right":
                 if directions[d] == "right":
                     matrix[i][j] = nodes[count]
                     count += 1
-                    if j < n-1:
+                    if j < n - 1:
                         j += 1
                     else:
                         i += 1
                         d = (d + 1) % len(directions)
-            
+                # case "down":
                 elif directions[d] == "down":
                     matrix[i][j] = nodes[count]
                     count += 1
-                    if i < m-1:
+                    if i < m - 1:
                         i += 1
                     else:
                         j -= 1
                         d = (d + 1) % len(directions)
-                
+                # case "left":
                 elif directions[d] == "left":
                     matrix[i][j] = nodes[count]
                     count += 1
@@ -63,6 +66,7 @@ class Solution(object):
                         i -= 1
                         d = (d + 1) % len(directions)
 
+                # case "up":
                 else:
                     matrix[i][j] = nodes[count]
                     count += 1
@@ -71,17 +75,29 @@ class Solution(object):
                     else:
                         j += 1
                         d = (d + 1) % len(directions)
-            
+
             else:
-                #we loop through the directions
+                # we loop through the directions
                 d = (d + 1) % len(directions)
-                i += 1
-                j += 1
+                if directions[d] == "right":
+                    j += 1
+                    i += 1
+                elif directions[d] == "down":
+                    i += 1
+                    j -= 1
+                elif directions[d] == "left":
+                    j -= 1
+                    i -= 1
+                else:
+                    i -= 1
+                    j += 1
+
+
                 print(matrix)
-        
+
         return matrix
 
-    def generateMatrix(self,m,n):
+    def generateMatrix(self, m, n):
         matrix = []
         for i in range(m):
             rows = []
@@ -89,14 +105,18 @@ class Solution(object):
                 rows.append(-1)
             matrix.append(rows)
         return matrix
-    
+
+
 def main():
     solution = Solution()
-    matrix = solution.generateMatrix(3,5)
+    m = 4
+    n = 5
+    matrix = solution.generateMatrix(m,n)
     print(matrix)
-    lisst = [3,0,2,6,8,1,7,9,4,2,5,5,0]
+    #lisst = [3, 0, 2, 6, 8, 1, 7, 9, 4, 2, 5, 5, 0]
+    lisst = [515,942,528,483,20,159,868,999,474,320,734,956,12,124,224,252,909,732]
     head = create_linked_list(lisst)
-    '''
+    """
     nodes = []
     for i in range (len(head)-1):
         node = ListNode(head[i], head[i+1])
@@ -105,20 +125,24 @@ def main():
     nodes.append(final_node)
     #for node in nodes:
         #print(node.val,node.next)
-        '''
-    solution = solution.spiralMatrix(3,5,head)
-    
+        """
+    solution = solution.spiralMatrix(m,n, head)
+
     print(solution)
+
+
 def create_linked_list(values):
-    #create a linked list form a list of values
+    # create a linked list form a list of values
     if not values:
         return None
-    #initialise head with first element
+    # initialise head with first element
     head = ListNode(values[0])
     current = head
     for value in values[1:]:
         current.next = ListNode(value)
         current = current.next
     return head
+
+
 if __name__ == "__main__":
     main()
